6eb4f26fc31f82ab47d66f4737f90ec8a7c67a41
AlexanderDegterev <Alexander.1@mail.ru>
2018-06-07 10:13:45 +0300

    lesson -19

---
+++ Androinlessons/P0102_ActivityListener/src/main/java/ru/startandroid/p0102_activitylistener/MainActivity.java
@@ -0,0 +1,49 @@
+package ru.startandroid.p0102_activitylistener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+
+    public void onClickStart(View v) {
+        // действия при нажатии на кнопку
+        tvOut.setText("Нажата кнопка START");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        // найдем View-элементы
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+
+        // присваиваем обработчик кнопкам
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
+        }
+
+    public void onClick(View v) {
+        // по id определеяем кнопку, вызвавшую этот обработчик
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                tvOut.setText("Нажата кнопка ОК");
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                tvOut.setText("Нажата кнопка Cancel");
+                break;
+        }
+    }
+}
